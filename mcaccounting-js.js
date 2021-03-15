c166d57b5a81d46ebf833b5f494374a8b3002461
ZakiZughbi <zakizughbi@gmail.com>
2021-03-15 16:39:06 -0400

    init commit

---
+++ mcaccounting-js.js
@@ -0,0 +1,34 @@
+var openBtn = document.getElementById('bars');
+var nav = document.getElementById('mySidenav');
+var closeBtn = document.getElementById('closebtn');
+var scrollNav = document.getElementById('navbar');
+var boxes = document.querySelectorAll('.box');
+//querySelectorAll
+//getElementsByClassName
+openBtn.addEventListener('click', function(){
+    nav.style.width = "200px";
+});
+
+
+closeBtn.addEventListener('click', function(){
+    nav.style.width = "0px";
+});
+
+window.onscroll = function(){myFunction();};
+
+function myFunction(){
+    if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 100){
+        scrollNav.style.top = "0";
+    }else{
+        scrollNav.style.top = "-50px";
+    }
+}
+
+
+boxes.forEach(event => event.addEventListener('click', function(){
+    if(event.style.transform == ""){
+        event.style.transform="rotateY(180deg)";
+    } else{
+        event.style.transform="";
+    }
+}));
